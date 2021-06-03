3672f6671900e425b8579a4fc779a3d0071dda4e
Tom Rishworth <tom@webscope.co.nz>
2021-06-03 14:01:10 +1200

    Init commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,7 +12,7 @@ import {
 
 export const query = graphql`
   query {
-    shopifyCollection(handle: { eq: "frontpage" }) {
+    shopifyCollection(handle: { eq: "latest-products" }) {
       products {
         ...ProductCard
       }
